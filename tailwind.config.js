34c8a613b693e74886b372fc21febbed82c8ae2a
rajaboff007 <asadbekrajaboff1337@gmail.com>
2024-10-27 21:43:11 +0500

    home page completed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        mainShadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'
+      },
+      colors: {
+        mainColor: '#FF6633'
+      }
+    },
+  },
+  plugins: [],
+}
+
